73739f19e73b7f18513fbef2a2b1ef1c2d319e46
hovojuharyan <36985179+hovojuharyan@users.noreply.github.com>
2018-10-14 18:17:25 +0400

    Update StomotologyImagesRepository.java

--- src/main/java/com/example/demo/repository/StomotologyImagesRepository.java
+++ src/main/java/com/example/demo/repository/StomotologyImagesRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.StomotologyImages;
 
-public interface StomotologyImagesRepository extends JpaRepository<StomotologyImagesRepository,Integer> {
+public interface StomotologyImagesRepository extends JpaRepository<StomotologyImages,Integer> {
 }
